2404e9240aa033cc1f38c197bf7e324c75a86d60
Muhammad Hilman <muhammadhilman27@users.noreply.github.com>
2015-11-11 15:05:58 +0700

    update 11-10-2015

--- Engine/Board.cs
+++ Engine/Board.cs
@@ -27,6 +27,27 @@ namespace Engine
         }
       }
       count=start=end=0;
+      while(count<8){
+        start=r.nextInt(98)+1;
+        end=r.nextInt(98)+1;
+        if((start>end)&&isValidSnakeLadder(tiles[start],tiles[end])){
+          tiles[start].setSnake(true);
+          tiles[start].setSnakeladder(new SnakeLadder(start,end));
+          tiles[end].setSnakeladder(new SnakeLadder(start,end));
+          count++;
+      }
+    }
+  /*    private boolean isValidSnakeLadder(Tile t1,Tile t2){
+        if(t1.getSnakeladder()!=null||t2.getSnakeladder()!=null){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }*/
+
+    public Tile[] getTiles() {
+        return tiles.GetNumber;
     }
   }
 }
