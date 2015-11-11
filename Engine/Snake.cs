3d30fea99aea85e3f06cf5daae4777d123878e5e
StephanieTania <7314072@student.unpar.ac.id>
2015-11-11 14:39:38 +0700

    Meng-extend kelas Snake dari kelas Teleporter dan implementasi kelas
    Menambah constructor yang memanggil constructor parent class, mengimplementasikan method MovePlayer

--- Engine/Snake.cs
+++ Engine/Snake.cs
@@ -1,7 +1,15 @@
 namespace Engine
 {
-  class Snake
+  class Snake:Teleporter//tail adalah titik akhir (end)
   {
+    public Snake(int head,int tail):base(head,tail)
+    {
+      
+    }
     
+    public override void MovePlayer(Player p)
+    {
+      //set posisi pemain pada tail
+    }
   }
 }
