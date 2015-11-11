dde97ee97972a46e1eefddf399b6eb3adf4b8d44
Lydia Febtriani <7314044@student.unpar.ac.id>
2015-11-11 14:31:16 +0700

    Menambah atribut, setter-getternya, & konstraktor

--- Engine/Tile.cs
+++ Engine/Tile.cs
@@ -2,6 +2,36 @@ namespace Engine
 {
   class Tile
   {
+    private int number; //nomor Tile
+    private Teleporter isSnake, isLadder;
     
+    public Tile(int num){
+      this.number=num;
+      this.isSnake=this.isLadder=null;
+    }
+    
+    public void SetNumber(int num){
+      this.number=num;
+    }
+    
+    public void SetIsSnake(Teleporter isSnake){
+      this.isSnake=isSnake;
+    }
+    
+    public void SetIsLadder(Teleporter isLadder){
+      this.isLadder=isLadder;
+    }
+    
+    public int GetNumber(){
+      return this.number;
+    }
+    
+    public Teleporter IsSnake(){
+      return this.isSnake;
+    }
+    
+    public Teleporter IsLadder(){
+      return this.isLadder;
+    }
   }
 }
