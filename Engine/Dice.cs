2a923b86665ec8ca2f0409ceec783376077440fd
hendrotanoto17 <htanz@yahoo.com>
2015-11-11 15:43:20 +0700

    testing

--- Engine/Dice.cs
+++ Engine/Dice.cs
@@ -1,7 +1,19 @@
 namespace Engine
-{
+
   class Dice
   {
-    
+    private int sides;
+    public Dice(int sides) {
+        this.sides = sides;
+    }
+  //constructor tanpa parameter
+    public Dice() {
+        this.sides=6;
+    }
+    // random dari 1-6
+    public int rollDice(){
+        Random r=new Random();
+        int dices = rnd.Next(1, 7);
+        return dices;
   }
 }
