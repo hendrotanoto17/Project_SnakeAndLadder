239518598169064e07abbaf48080808f938cb6ce
Ihsan Fajari <ihsan.fajari@gmail.com>
2015-11-11 14:55:42 +0700

    Constructing Game.cs

--- Engine/Game.cs
+++ Engine/Game.cs
@@ -1,1 +1,22 @@
-
+namespace Engine
+{
+  //kelas controller.
+  //yang menjalankan semua proses yang diperlukan untuk menjalankan game ini.
+  class Game{
+    
+    Board papan ;
+    Player[] players ;
+    
+    //konstruktor
+    public Game(Board b, Player[] pemain){
+      this.papan = b;
+      this.players = pemain;
+    }
+    
+    public void setPlayerLocation(Player p){
+      papan.setPlayer(p.);
+      
+    }
+  }
+  
+}
