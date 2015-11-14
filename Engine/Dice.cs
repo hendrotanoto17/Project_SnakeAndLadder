c9da950f22ccd3822ebf0d03b936fc6aaa48fa8f
hendrotanoto17 <htanz@yahoo.com>
2015-11-14 16:09:02 +0700

    Update dice

--- Engine/Dice.cs
+++ Engine/Dice.cs
@@ -1,7 +1,32 @@
 namespace Engine
 {
-  class Dice
-  {
+  public class Dice {
+    private int sides;
+
+    public Dice(int sides) {
+        this.sides = sides;
+    }
+
+    public Dice() {
+        this.sides=6;
+    }
     
-  }
+    public int rollDice(){
+        Random rnd = new Random();
+        int hasil = rnd.Next(1,7)
+        
+       
+          if(hasil == 6){
+            for(int a =2 ;a<=0;a--){
+            return Player.move(hasil)++
+          }
+          }
+          else{
+          return Player.move();
+        }
+          return Player.move(hasil);
+        }
+        
+    }
 }
+
